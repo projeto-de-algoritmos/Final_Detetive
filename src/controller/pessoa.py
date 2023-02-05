f0dfc83fa63ede387ee74830335ab6bb65050bd5
giovannabbottino <giovannabbottino@gmail.com>
2023-02-05 13:38:52 -0300

    adiciona logica de butao de pessoa

--- src/controller/pessoa.py
+++ src/controller/pessoa.py
@@ -27,46 +27,36 @@ class PessoaController:
                         text_appear +=1
                     last_key_check_time = current_time
             else: 
-                print('a fazer')
+                botoes_list = [
+                    Button('Qual a sua relação com a vitima?', pygame, screen, x = 30, button_width=100, font_size=20),
+                    Button('Onde estava na noite passada?', pygame, screen, x = 100, font_size=20),
+                    Button('Sabe quem pode ser um suspeito?', pygame, screen, x = 170, button_width=100, font_size=20),
+                    Button('Coletar amostra de DNA', pygame, screen, x = 210, button_width=70, font_size=20),
+                    Button('Prender', pygame, screen, x = 330, button_width=60, font_size=20),
+                    Button('Voltar', pygame, screen, x = 400, button_width=60, font_size=20)
+                ]
+                
 
-            self.clicked(botoes_list)
             self.main_page.update()
+            for b in botoes_list:
+                    b.update()
+            self.clicked(botoes_list)
             self.pygame.display.update()
-        # print(f'[Detetive] Olá! {self.pessoa.nome}, como você está?')
-        # print(f'[{self.pessoa.nome}] {self.pessoa.saldacao}')
-        # print(f'[Detetive] Estou aqui pelo caso de Maria, que foi assassinada na noite passada na praça de São Carlos as 21h')
-        # print(f'[{self.pessoa.nome}] {self.pessoa.reacao}')
-        # self.acoes()
-
-    def acoes(self) -> None:
-        escolha = None
-        while escolha != 6 and not self.preso:
-            print("<1> Qual a sua relação com a vitima?")
-            print("<2> Onde estava na noite passada?")
-            print("<3> Sabe quem pode ser um suspeito?")
-            print("<4> Coletar amostra de DNA")
-            print("<5> Prender")
-            print("<6> Voltar")
-            try:
-                escolha = int(input())
-                if (escolha < 1 or escolha > 6):
-                    raise KeyError
-                self.respostas(escolha)
-            except:
-                print("Precisa ser um número")
 
     def respostas(self, escolha: int) -> None:
-        if escolha == 1:
-            print(f'[{self.pessoa.nome}] {self.pessoa.relacao}')
+        if escolha == 0:
+            self.main_page.text.text = f'[{self.pessoa.nome}] {self.pessoa.relacao}'
+        elif escolha == 1:
+            self.main_page.text.text = f'[{self.pessoa.nome}] {self.pessoa.alibi}'
         elif escolha == 2:
-            print(f'[{self.pessoa.nome}] {self.pessoa.alibi}')
+            self.main_page.text.text = f'[{self.pessoa.nome}] {self.pessoa.suspeito}'
         elif escolha == 3:
-            print(f'[{self.pessoa.nome}] {self.pessoa.suspeito}')
+            self.main_page.text.text = f'[Detetive] Compatibilidade com o DNA {self.dna.compara_dna(self.pessoa.dna)}%'
         elif escolha == 4:
-            print(f'[Detetive] Compatibilidade com o DNA {self.dna.compara_dna(self.pessoa.dna)}%')
-        elif escolha == 5:
-            print(f'Você prendeu {self.pessoa.nome}.')
+            self.main_page.text.text = f'Você prendeu {self.pessoa.nome}.'
             self.preso = True
+        elif escolha == 5:
+            self.run = False
 
     def changetext(self, count: int) -> None:
         if count == 0:
@@ -88,6 +78,6 @@ class PessoaController:
                 self.pygame.quit()
                 raise SystemExit
             if event.type == self.pygame.MOUSEBUTTONUP:
-                for b in botoes_list:
+                for i, b in enumerate(botoes_list):
                     if b.button.collidepoint(event.pos):
-                        pass
+                        self.respostas(i)
